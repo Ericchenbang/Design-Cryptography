f74e7dee88f2dd7bae92ccdda5a1dff1210c7009
Ericchenbang <ericchenbsng@gmail.com>
2024-10-05 14:18:32 +0800

    Add caesar cipher.

---
+++ caesar-Encryption.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define inputL 100
+
+void caesar(char *p, char *q);
+
+int main(void){
+	int move;
+	printf("Please input the number you want to move(for example: 1 is for a -> b): ");
+ 	scanf("%d", &move);
+	if (move >= 26 || move <= -26){
+		move %= 26;
+	}
+	printf("Move %d!\n", move);
+	printf("Please input the plaintext: ");
+	getchar();
+
+	char input[inputL], ciphertext[inputL];
+	fgets(input, 100, stdin);
+	input[strcspn(input, "\n")] = 0;
+	
+	caesar(input, ciphertext);
+
+	char *c = ciphertext;
+	// printf("%s\n", input);
+	printf("The ciphertext: %s\n", ciphertext);
+	
+
+	return 0;
+}
+
+void caesar(char *p, char *q){
+	int i = 0;
+	while(p[i] != '\0'){
+		if (p[i] == 32){
+			q[i++] = 32;
+			continue;
+		}
+		q[i] = 65 + ((p[i] - 97 + 3) % 26);
+		i++;
+	}
+	q[i] = '\0';
+}
+
